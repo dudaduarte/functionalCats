b0a96ec38e25ff33a9f52a27b3e0eabf48dcc861
Duda Duarte <dudammduarte@yahoo.com.br>
2020-08-24 20:54:15 -0300

    first version working

---
+++ index.js
@@ -0,0 +1,36 @@
+const CDN = s => `https://cdnjs.cloudflare.com/ajax/libs/${s}`
+const ramda = CDN('ramda/0.25.0/ramda.min')
+const jquery = CDN('jquery/3.0.0-rc1/jquery.min')
+
+requirejs.config({ paths: { ramda, jquery } })
+requirejs(['jquery', 'ramda'], ($, { path, pipe, curry, map, prop }) => {
+  const Impure = {
+    getJSON: curry((callback, url) => $.getJSON(url, callback)),
+    setHtml: curry((sel, html) => $(sel).html(html)),
+    trace: curry((tag, x) => { console.log(tag, x); return x; }),
+  }
+
+  const createImg = src => $('<img />', { src })
+
+  const handleResponse = pipe(
+    prop('items'),
+    map(path(['media', 'm'])),
+    map(createImg),
+    Impure.setHtml('#js-main')
+  )
+
+  const host = 'api.flickr.com'
+  const route = '/services/feeds/photos_public.gne'
+  const query = t => `?tags=${t}&format=json&jsoncallback=?`
+  const url = t => `https://${host}${route}${query(t)}`
+
+  const app = pipe(
+    url,
+    Impure.getJSON(
+      pipe(
+        handleResponse,
+        Impure.trace('response')
+      ))
+  )
+  app('cats')
+})
